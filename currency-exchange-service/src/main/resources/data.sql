0e6582578b850961b6fa908970c1c0eada211a6e
Shounak Bose <shoun.official@gmail.com>
2021-08-10 10:41:52 +0530

    v0.0.2

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into currency_exchange
+(id, conversion_multiple,currency_from,currency_to)
+values (10001,65,'USD','INR');
+INSERT INTO currency_exchange (id, conversion_multiple,currency_from,currency_to) VALUES (10002,75,'EUR','INR');
+INSERT INTO currency_exchange (id, conversion_multiple,currency_from,currency_to) VALUES (10003,25,'AUD','INR');
